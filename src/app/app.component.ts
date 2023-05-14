18e6cca3828eaa180e9b8976791b02fa0a9a696f
Andrew <andrewpullen1@gmail.com>
2023-05-14 18:21:53 -0400

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MenuItem } from './menu-item';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,51 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Menu';
-}
+
+  menu: MenuItem[] = [
+    {
+      name: "Chicken Fingers",
+      category: "dinner",
+      price: 11.99
+    },
+    {
+      name: "Pizza",
+      category: "dinner",
+      price: 11.99
+    },
+    {
+      name: "Wings",
+      category: "sides",
+      price: 8.99
+    },
+    {
+      name:"Breadsticks",
+      category:"sides",
+      price: 4.99
+    },
+    {
+      name:"Caesar Salad",
+      category:"salads",
+      price: 5.99
+    },
+    {
+      name:"Cinnamon Roll",
+      category:"dessert",
+      price: 8.99
+    }
+  ];
+
+  getByCategory(cat: string): MenuItem[] {
+    return this.menu.filter(item => item.category === cat);
+  };
+
+  getCategories(): string[]{
+    const categories = this.menu.map(item => item.category)
+    console.log(categories);
+      // return Array.from(new Set(categories));
+      return [...new Set(categories)]
+  };
+
+};
+  
+  
